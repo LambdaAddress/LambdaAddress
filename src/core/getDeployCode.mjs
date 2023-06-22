4842cc13cd1e43cd155142893e6677b2b2e244b8
trinidadalpha <>
2023-06-22 18:44:41 -0400

    Pop createdBy from stack in SafeCreate3

--- src/core/getDeployCode.mjs
+++ src/core/getDeployCode.mjs
@@ -4,6 +4,9 @@
  * @param {string} generatedBy Address of the account generating the NFTAddress
  */
 export default function getDeployCode(generatedBy) {
-    return '0x73' + generatedBy.replace('0x', '') +
+    return '' +
+    '0x73' + // PUSH20
+    generatedBy.replace('0x', '') + 
+    '50' + // POP
     '67363d3d37363d34f03d5260086018f3'
 }
