6cb7f4993e92dd8bb61ccd06dab4d0f4e4ec0ecf
trinidadalpha <>
2023-07-12 15:57:58 -0400

    Adds comments and format SafeDeployer

--- src/contracts/deployers/SafeDeployer.sol
+++ src/contracts/deployers/SafeDeployer.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import {Errors} from "../Errors.sol";
 import {GnosisSafeProxy} from "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxy.sol";
 import {GnosisSafeProxyFactory} from "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
 import {Registrar} from "../Registrar.sol";
 
-/// @title 
-/// @notice 
+/// @title SafeDeployer
+/// @notice Deploys a Gnosis Safe to a NFT Address
 contract SafeDeployer is GnosisSafeProxyFactory {
   Registrar private _registrar;
 
@@ -16,23 +15,29 @@ contract SafeDeployer is GnosisSafeProxyFactory {
     _registrar = registrar;
   }
 
-  /// @notice 
-  /// @param tokenId t
-  /// @param singleton  s
-  /// @param initializer i
-  function deploy(uint256 tokenId, address singleton, bytes memory initializer) public returns (GnosisSafeProxy proxy) {
-    bytes memory deploymentData = abi.encodePacked(type(GnosisSafeProxy).creationCode, uint256(uint160(singleton)));
+  /// @notice Deploys a Gnosis Safe to a Lambda Address
+  /// @param tokenId ID of the NFT Address
+  /// @param singleton Address of Gnosis Safe singleton contract
+  /// @param initializer Payload for message call sent to new proxy contract
+  function deploy(
+    uint256 tokenId,
+    address singleton,
+    bytes memory initializer
+  ) public returns (GnosisSafeProxy proxy) {
+    bytes memory deploymentData = abi.encodePacked(
+      type(GnosisSafeProxy).creationCode,
+      uint256(uint160(singleton))
+    );
     _registrar.deploy(tokenId, deploymentData);
     if (initializer.length > 0) {
       // solhint-disable-next-line no-inline-assembly
       assembly {
         if eq(call(gas(), proxy, 0, add(initializer, 0x20), mload(initializer), 0, 0), 0) {
-            revert(0, 0)
+          revert(0, 0)
         }
       }
     }
     proxy = GnosisSafeProxy(payable(address(uint160(tokenId))));
     emit ProxyCreation(proxy, singleton);
-  } 
-
+  }
 }
