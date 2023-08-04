d10ac342e79fc71fdd59743bb27d2590646f02ff
trinidadalpha <>
2023-08-04 17:06:31 -0400

    Adds config file

---
+++ src/frontend/components/deployers/GnosisSafeDeployer/config.js
@@ -0,0 +1,16 @@
+export default {
+    "contracts": {
+        // Ethereum 
+        "1": {
+            "GnosisSafeImplementation": "0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4"
+        },
+        // Hardhat localhost
+        "31337": {
+            "GnosisSafeImplementation": "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6"
+        },
+        // Goerli
+        "5": {
+            "GnosisSafeImplementation": "0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4"
+        }
+    }
+}
