29a54334b2af1362cf973399ede2c55ae1b5c850
MrElement01 <>
2023-09-23 14:13:59 -0400

    Adds Arbitrum network info

--- src/config/index.js
+++ src/config/index.js
@@ -23,7 +23,6 @@ const supportedNetworks = {
         graphUrl: 'https://api.studio.thegraph.com/query/47325/lambdaaddress-optimism/version/latest',
         contracts: optimism
     },
-    /*
     42161: {
         chainId: 42161,
         name: 'Arbitrum',
@@ -36,7 +35,7 @@ const supportedNetworks = {
         rpcUrls: ['https://arb1.arbitrum.io/rpc'],
         blockExplorerUrls: ['https://arbiscan.io/'],
         isTestnet: false
-    },*/
+    },
     11155111: { 
         name: 'Sepolia',
         chainId: 11155111,
