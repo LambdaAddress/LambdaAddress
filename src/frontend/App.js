a0bed6cec65e7b131920ce8b65bbf54ad21c99b3
MrElement01 <>
2023-10-21 16:23:01 -0400

    Adds styled spinner

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,6 +2,8 @@ import { MainContextProvider } from './MainContext'
 import { lazy, useEffect, Suspense } from 'react'
 import ReactGA from "react-ga"
 import { Routes, Route, Navigate, HashRouter } from 'react-router-dom'
+import Spinner from './components/Spinner'
+import styled from '@emotion/styled'
 
 const Home = lazy(() => import('./pages/Home'))
 const Mint = lazy(() => import('./pages/Mint'))
@@ -16,20 +18,34 @@ export default function App() {
   }, [])
 
   return (
-    <MainContextProvider>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<RouteLoading page={<Home />} />} />
-          <Route path="/mint" element={<RouteLoading page={<Mint />} />} />
-          <Route path="/addresses" element={<RouteLoading page={<AddressList />} />} />
-          <Route path="/svg" element={<RouteLoading page={<LambdaAddressSvg />} />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </HashRouter>
-    </MainContextProvider>
+    <Main>
+      <MainContextProvider>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<RouteLoading page={<Home />} />} />
+            <Route path="/mint" element={<RouteLoading page={<Mint />} />} />
+            <Route path="/addresses" element={<RouteLoading page={<AddressList />} />} />
+            <Route path="/svg" element={<RouteLoading page={<LambdaAddressSvg />} />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </HashRouter>
+      </MainContextProvider>
+    </Main>
   )
 }
 
 function RouteLoading({ page }) {
-  return <Suspense fallback={<div>Loading...</div>}>{page}</Suspense>
+  return <Suspense fallback={<LoadingIcon size={2} />}>{page}</Suspense>
 }
+
+const Main = styled.div({
+  background: 'linear-gradient(135deg, rgb(111, 94, 239), rgb(32, 18, 60)) center center / cover transparent',
+  minHeight: '100vh'
+})
+
+const LoadingIcon = styled(Spinner)({
+  display: 'block',
+  position: 'absolute',
+  left: 'calc(50% - 20px)',
+  top: 'calc(50% - 40px)'
+})
